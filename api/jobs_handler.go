0c8c72ede3a71ffb2fc967c80702f67108c1c869
anfelo <af.osorio1341@gmail.com>
2021-09-10 15:46:28 -0500

    Added ci deployment action

--- api/jobs_handler.go
+++ api/jobs_handler.go
@@ -14,9 +14,9 @@ type JobHandler struct {
 func (h *JobHandler) Seed() http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
 		var ss []courses.Subject
-		s1 := courses.Subject{Title: "Software Fundamentals", Slug: "software-fundamentals", ImgURL: "software-fundamentals.png"}
-		s2 := courses.Subject{Title: "Python", Slug: "python", ImgURL: "python.png"}
-		s3 := courses.Subject{Title: "VueJS", Slug: "vuejs", ImgURL: "vue.png"}
+		s1 := courses.Subject{Title: "Software Fundamentals", Slug: "software-fundamentals", ImgURL: "software-fundamentals.svg"}
+		s2 := courses.Subject{Title: "Python", Slug: "python", ImgURL: "python.svg"}
+		s3 := courses.Subject{Title: "Vue", Slug: "vue", ImgURL: "vue.svg"}
 		ss = append(ss, s1, s2, s3)
 
 		for i, s := range ss {
@@ -33,28 +33,28 @@ func (h *JobHandler) Seed() http.HandlerFunc {
 			Title:       "Design Patterns",
 			Description: "Design patterns are typical solutions to common problems in software design. Each pattern is like a blueprint that you can customize to solve a particular design problem in your code.",
 			Slug:        "design-patterns",
-			ImgURL:      "design-patterns.png",
+			ImgURL:      "software-fundamentals.svg",
 		}
 		c2 := courses.Course{
 			SubjectID:   ss[0].ID,
 			Title:       "Algorithms",
 			Description: "In mathematics and computer science, an algorithm is a finite sequence of well-defined, computer-implementable instructions, typically to solve a class of specific problems or to perform a computation. Algorithms are always unambiguous and are used as specifications for performing calculations, data processing, automated reasoning, and other tasks.",
 			Slug:        "algorithms",
-			ImgURL:      "algorithms.png",
+			ImgURL:      "software-fundamentals.svg",
 		}
 		c3 := courses.Course{
 			SubjectID:   ss[1].ID,
 			Title:       "Python Practical Guide",
 			Description: "Python is an interpreted high-level general-purpose programming language. Its design philosophy emphasizes code readability with its use of significant indentation. Its language constructs as well as its object-oriented approach aim to help programmers write clear, logical code for small and large-scale projects.",
 			Slug:        "python-practical-guide",
-			ImgURL:      "python.png",
+			ImgURL:      "python.svg",
 		}
 		c4 := courses.Course{
 			SubjectID:   ss[2].ID,
 			Title:       "Vue Composition Api",
 			Description: "What makes the Vue 3 Composition API so much better than the Options API is code sharing. Inside the setup hook of a component, we can group parts of our code by logical concern. We then can extract pieces of reactive logic and share the code with other components.",
 			Slug:        "vue-composition-api",
-			ImgURL:      "python.png",
+			ImgURL:      "vue.svg",
 		}
 		cc = append(cc, c1, c2, c3, c4)
 
