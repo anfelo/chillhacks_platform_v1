2fce67e9033d5f2f2405aae5472bfe8efb5bbd45
anfelo <af.osorio1341@gmail.com>
2021-10-04 22:15:46 -0500

    Changed Auth to user Authorization token

--- api/middlewares.go
+++ api/middlewares.go
@@ -2,18 +2,28 @@ package api
 
 import (
 	"context"
+	"fmt"
 	"net/http"
 
+	"github.com/anfelo/chillhacks_platform/courses"
 	"github.com/anfelo/chillhacks_platform/utils/errors"
 	"github.com/anfelo/chillhacks_platform/utils/http_utils"
-	"github.com/google/uuid"
 )
 
 func (h *Handler) withUser(next http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		id, _ := h.sessions.Get(r.Context(), "user_id").(uuid.UUID)
+		if r.Header["Authorization"] == nil {
+			next.ServeHTTP(w, r)
+			return
+		}
+		username, tokenErr := VerifyJWT(r.Header["Authorization"][0])
+		if tokenErr != nil {
+			next.ServeHTTP(w, r)
+			return
+		}
+		fmt.Println(username)
 
-		user, err := h.store.User(id)
+		user, err := h.store.UserByUsername(username)
 		if err != nil {
 			next.ServeHTTP(w, r)
 			return
@@ -26,10 +36,12 @@ func (h *Handler) withUser(next http.Handler) http.Handler {
 
 func (h *Handler) authRequest(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		s := GetSessionData(h.sessions, r.Context())
-		if s.LoggedIn {
-			next.ServeHTTP(w, r)
-			return
+		if r.Context().Value("user") != nil {
+			user := r.Context().Value("user").(courses.User)
+			if user.Username != "" {
+				next.ServeHTTP(w, r)
+				return
+			}
 		}
 		restErr := errors.NewUnauthorizedError("unauthorized request")
 		http_utils.RespondJson(w, restErr.Status, restErr)
@@ -38,10 +50,12 @@ func (h *Handler) authRequest(next http.HandlerFunc) http.HandlerFunc {
 
 func (h *Handler) adminRequest(next http.HandlerFunc) http.HandlerFunc {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
-		s := GetSessionData(h.sessions, r.Context())
-		if s.User.Role == "admin" {
-			next.ServeHTTP(w, r)
-			return
+		if r.Context().Value("user") != nil {
+			user := r.Context().Value("user").(courses.User)
+			if user.Role == "admin" {
+				next.ServeHTTP(w, r)
+				return
+			}
 		}
 		restErr := errors.NewUnauthorizedError("unauthorized request")
 		http_utils.RespondJson(w, restErr.Status, restErr)
